ce37a7346530a2bac73d758eabc92824c957c392
Hinacle <workerlibai@163.com>
2022-08-16 10:25:16 +0800

    no message

--- base/src/main/java/com/hinacle/base/util/BitmapUtil.kt
+++ base/src/main/java/com/hinacle/base/util/BitmapUtil.kt
@@ -1,6 +1,13 @@
 package com.hinacle.base.util
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ImageView
 import java.io.File
 import java.io.FileOutputStream
 
@@ -38,4 +45,83 @@ fun Bitmap.writeToFile(targetPath: String): String? {
         e.printStackTrace()
         null
     }
+}
+
+
+/**
+ * 安全创建Bitmap，如果产生了OOM，可以主动GC后再尝试。
+ * https://github.com/QMUI/QMUI_Android
+ * @param width : Bitmap宽度
+ * @param height : Bitmap高度度
+ * @param retryCount : 重试次数，默认一次
+ * @param config : Bitmap.Config，默认为ARGB_8888
+ * @return 创建Bitmap成功返回Bitmap，否则返回null。
+ */
+fun createBitmapSafely(
+    width: Int,
+    height: Int,
+    retryCount: Int = 1,
+    config: Bitmap.Config = Bitmap.Config.ARGB_8888
+): Bitmap? = try {
+    Bitmap.createBitmap(width, height, config)
+} catch (e: OutOfMemoryError) {
+    e.printStackTrace()
+    when (retryCount > 0) {
+        true -> {
+            System.gc()
+            createBitmapSafely(width, height, retryCount, config)
+        }
+        false -> null
+    }
+}
+
+/**
+ * 将View转换为Bitmap
+ * @param  scale: 生成的Bitmap相对于原View的大小比例，范围为0~1.0
+ */
+fun viewToBitmap(view: View, scale: Float = 1.0F): Bitmap? = when (view) {
+    is ImageView -> {
+        val drawable: Drawable = view.drawable
+        (drawable as BitmapDrawable).bitmap
+    }
+    else -> {
+        view.clearFocus()
+        val bitmap =
+            createBitmapSafely((view.width * scale).toInt(), (view.height * scale).toInt())
+        if (bitmap != null) {
+            val canvas = Canvas()
+            synchronized(canvas) {
+                with(canvas) {
+                    setBitmap(bitmap)
+                    save()
+                    drawColor(Color.WHITE)
+                    scale(scale, scale)
+                    view.draw(canvas)
+                    restore()
+                    setBitmap(null)
+                }
+            }
+        }
+        bitmap
+    }
+}
+
+// 5. Drawable----> Bitmap
+fun drawableToBitmap(drawable: Drawable): Bitmap? {
+
+    // 获取 drawable 长宽
+    val width = drawable.intrinsicWidth
+    val heigh = drawable.intrinsicHeight
+    drawable.setBounds(0, 0, width, heigh)
+
+    // 获取drawable的颜色格式
+    val config =
+        if (drawable.opacity != PixelFormat.OPAQUE) Bitmap.Config.ARGB_8888 else Bitmap.Config.RGB_565
+    // 创建bitmap
+    val bitmap = Bitmap.createBitmap(width, heigh, config)
+    // 创建bitmap画布
+    val canvas = Canvas(bitmap)
+    // 将drawable 内容画到画布中
+    drawable.draw(canvas)
+    return bitmap
 }
--- base/src/main/java/com/hinacle/base/util/ViewUtil.kt
+++ base/src/main/java/com/hinacle/base/util/ViewUtil.kt
@@ -1,9 +1,12 @@
 package com.hinacle.base.util
 
+import android.graphics.Bitmap
 import android.view.View
+import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
+import android.widget.TextView
 import java.util.concurrent.TimeUnit
 
 /**
@@ -60,3 +63,185 @@ fun View.fadeIn(duration: Long = 500) {
         setDuration(duration)
     })
 }
+
+
+/**
+ * 当前View是否可见
+ */
+val View.isVisible
+    get() = visibility == View.VISIBLE
+
+/**
+ * 当前View是否不可见
+ */
+val View.isInvisible
+    get() = visibility == View.INVISIBLE
+
+/**
+ * 当前View是否隐藏
+ */
+val View.isGone
+    get() = visibility == View.GONE
+
+/**
+ * 将View设置为隐藏
+ */
+fun View.setGone() {
+    if (visibility != View.GONE) {
+        visibility = View.GONE
+    }
+}
+
+/**
+ * 将View设置为可见
+ */
+fun View.setVisible() {
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
+}
+
+/**
+ * 将View设置为不可见
+ */
+fun View.setInvisible() {
+    if (visibility != View.INVISIBLE) {
+        visibility = View.INVISIBLE
+    }
+}
+
+/**
+ * 设置View的宽度
+ * @param width: 宽度值，单位为px
+ */
+fun View.setWidth(width: Int) {
+    layoutParams = layoutParams.apply {
+        this.width = width
+    }
+}
+
+/**
+ * 设置View的高度
+ * @param height: 高度值，单位为px
+ */
+fun View.setHeight(height: Int) {
+    layoutParams = layoutParams.apply {
+        this.height = height
+    }
+}
+
+/**
+ * 设置View的宽度和高度
+ * @param width: 宽度值，单位为px
+ * @param height: 高度值，单位为px
+ */
+fun View.setWidthAndHeight(width: Int, height: Int) {
+    layoutParams = layoutParams.apply {
+        this.width = width
+        this.height = height
+    }
+}
+
+/**
+ * 将View转换为Bitmap
+ * @param  scale: 生成的Bitmap相对于原View的大小比例，范围为0~1.0
+ */
+fun View.toBitmap(scale: Float = 1.0F): Bitmap? = viewToBitmap(this, scale)
+
+
+/**
+ * 设置View的padding
+ */
+fun View.setNewPadding(
+    left: Int = paddingLeft,
+    top: Int = paddingTop,
+    right: Int = paddingRight,
+    bottom: Int = paddingBottom
+) {
+    setPadding(left, top, right, bottom)
+}
+
+/**
+ * 测量View
+ */
+private fun View.measureView() {
+    var params = layoutParams
+    if (params == null) {
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+    val widthSpec = ViewGroup.getChildMeasureSpec(0, 0, params.width)
+    val heightSpec = if (params.height > 0) {
+        View.MeasureSpec.makeMeasureSpec(params.height, View.MeasureSpec.EXACTLY)
+    } else {
+        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+    }
+    measure(widthSpec, heightSpec)
+}
+
+/**
+ * 获取View的高度
+ * 如果是“math_parent”属性则无法获取，值为0。
+ */
+val View.viewHeight: Int
+    get() {
+        measureView()
+        return measuredHeight
+    }
+
+/**
+ * 获取View的宽度
+ * 如果是“math_parent”属性则无法获取，值为0。
+ */
+val View.viewWidth: Int
+    get() {
+        measureView()
+        return measuredWidth
+    }
+
+/**
+ * 获取TextView的String内容
+ */
+val TextView.textString: String
+    get() = text.toString()
+
+/**
+ * 获取TextView的String内容长度
+ */
+val TextView.textLength: Int
+    get() = text.length
+
+/**
+ * 判断TextView的内容是否为空
+ */
+val TextView.isTextEmpty: Boolean
+    get() = text.isEmpty()
+
+/**
+ * 判断TextView的内容是否为null或空
+ */
+val TextView.isTextNullOrEmpty: Boolean
+    get() = text.isNullOrEmpty()
+
+/**
+ * 判断TextView的内容是否为非空
+ */
+val TextView.isTextNotEmpty: Boolean
+    get() = !isTextEmpty
+
+/**
+ * 判断TextView的内容是否为空白
+ */
+val TextView.isTextBlank: Boolean
+    get() = text.isBlank()
+
+/**
+ * 判断TextView的内容是否为null或空白
+ */
+val TextView.isTextNullOrBlank: Boolean
+    get() = text.isNullOrBlank()
+
+/**
+ * 判断TextView的内容是否为非空白
+ */
+val TextView.isTextNotBlank: Boolean
+    get() = text.isNotBlank()
