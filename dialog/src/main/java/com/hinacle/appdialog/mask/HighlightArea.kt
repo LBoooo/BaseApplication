ae002d1889f58d79df3ea2c620e5e31f6f5e7cff
Hinacle <workerlibai@163.com>
2022-08-17 13:57:33 +0800

    no message

--- dialog/src/main/java/com/hinacle/appdialog/mask/HighlightArea.kt
+++
@@ -1,9 +0,0 @@
-package com.hinacle.appdialog.mask
-
-import android.graphics.RectF
-
-data class HighlightArea(
-        var areaRect: RectF = RectF(),
-        var radiusX: Float = 0f,
-        var radiusY: Float = 0f
-)
--- dialog/src/main/java/com/hinacle/appdialog/mask/HighlightAreaType.kt
+++
@@ -1,6 +0,0 @@
-package com.hinacle.appdialog.mask
-
-enum class HighlightAreaType(val value: Int) {
-    TRANSPARENT_CUBE(0),//仅为透明高亮区域
-    BITMAP(1)//在高亮区域中绘制bitmap
-}
